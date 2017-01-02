1991904cb3ed060f99351ba25e4dcc2f1036c805
Jared <jaredlam86@gmail.com>
2017-01-02 12:18:53 +0800

    added production config

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     devtool: "eval-source-map",
     entry: __dirname + "/app/main.js",
     output: {
-        path: __dirname + "/public",
+        path: __dirname + "/build",
         filename: "bundle.js"
     },
     module: {
